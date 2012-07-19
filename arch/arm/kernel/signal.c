ce1613922ddfa8a44be3b4c65ea61630d22902e6
Al Viro <viro@zeniv.linux.org.uk>
2012-07-19 17:48:50 +0100

    BACKPORT: ARM: 7474/1: get rid of TIF_SYSCALL_RESTARTSYS
    (cherry pick from commit 6628521784d1da3b7354c6b6e8499e19ab46a3d1)
    
    just let do_work_pending() return 1 on normal local restarts and
    -1 on those that had been caused by ERESTART_RESTARTBLOCK (and 0
    is still "all done, sod off to userland now").  And let the asm
    glue flip scno to restart_syscall(2) one if it got negative from
    us...
    
    [will: resolved conflicts with audit fixes]
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Bug: 9674955
    Bug: 25773781
    Change-Id: I9dfff324249e930c6719c56ea9b27a37d36d277a

--- arch/arm/kernel/signal.c
+++ arch/arm/kernel/signal.c
@@ -626,7 +626,7 @@ static int do_signal(struct pt_regs *regs, int syscall)
 		 */
 		switch (retval) {
 		case -ERESTART_RESTARTBLOCK:
-			restart++;
+			restart -= 2;
 		case -ERESTARTNOHAND:
 		case -ERESTARTSYS:
 		case -ERESTARTNOINTR:
@@ -679,17 +679,13 @@ static int do_signal(struct pt_regs *regs, int syscall)
 		return 0;
 	}
 
-	if (unlikely(restart)) {
-		if (restart > 1)
-			set_thread_flag(TIF_SYSCALL_RESTARTSYS);
-		regs->ARM_pc = continue_addr;
-	}
-
 	/* If there's no signal to deliver, we just put the saved sigmask
 	 * back.
 	 */
 	if (test_and_clear_thread_flag(TIF_RESTORE_SIGMASK))
 		set_current_blocked(&current->saved_sigmask);
+	if (unlikely(restart))
+		regs->ARM_pc = continue_addr;
 	return restart;
 }
 
@@ -704,13 +700,14 @@ do_work_pending(struct pt_regs *regs, unsigned int thread_flags, int syscall)
 				return 0;
 			local_irq_enable();
 			if (thread_flags & _TIF_SIGPENDING) {
-				if (unlikely(do_signal(regs, syscall))) {
+				int restart = do_signal(regs, syscall);
+				if (unlikely(restart)) {
 					/*
 					 * Restart without handlers.
 					 * Deal with it without leaving
 					 * the kernel space.
 					 */
-					return 1;
+					return restart;
 				}
 				syscall = 0;
 			} else {
