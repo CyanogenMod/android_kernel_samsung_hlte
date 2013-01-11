334f2a3a24fe8b36b0f828b6c1c3b5dc46b2a3eb
Mark Rutland <mark.rutland@arm.com>
2013-01-11 14:32:33 +0000

    arm: arch_timer: add isbs to register accessors
    Without the isbs in arch_timer_get_cnt{p,v}ct the cpu may speculate
    reads and return stale values. This could be bad for code sensitive to
    changes in expected deltas between calls (e.g. the delay loop).
    
    Without isbs in arch_timer_reg_write the processor may reorder
    instructions around enabling/disabling of the timer or writing the
    compare value, which we probably don't want.
    
    This patch adds isbs to prevent those issues.
    
    Change-Id: I624261dec859f6aae6cd427a7b00ebcd2dfa5575
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/kernel/arch_timer.c
+++ arch/arm/kernel/arch_timer.c
@@ -308,6 +308,7 @@ static inline cycle_t notrace counter_get_cntpct_cp15(void)
 {
 	u32 cvall, cvalh;
 
+	isb();
 	asm volatile("mrrc p15, 0, %0, %1, c14" : "=r" (cvall), "=r" (cvalh));
 	return ((cycle_t) cvalh << 32) | cvall;
 }
@@ -329,6 +330,7 @@ static inline cycle_t notrace counter_get_cntvct_cp15(void)
 {
 	u32 cvall, cvalh;
 
+	isb();
 	asm volatile("mrrc p15, 1, %0, %1, c14" : "=r" (cvall), "=r" (cvalh));
 	return ((cycle_t) cvalh << 32) | cvall;
 }
