9eb68c90e0caa9fcb9881d30dc13fdcd17b7885d
darren.kang <darren.kang@lge.com>
2013-06-12 13:07:52 -0700

    video: msm: mdss: insert panel parameters for width and height
    for calculating dpi on HAL,FB should check panel physical size.
    so, this patch make panel driver parse dt and fill panel's width
    and height. and provide the information to FB.
    
    Change-Id: I0fbe12470c903e90c50e06ae2fbbe786187cc133
    Signed-off-by: darren.kang <darren.kang@lge.com>
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1106,6 +1106,8 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 		fix->line_length = var->xres * bpp;
 
 	var->yres = panel_info->yres;
+	var->width = panel_info->width;
+	var->height = panel_info->height;
 	var->xres_virtual = var->xres;
 	var->width = panel_info->width;
 	var->height = panel_info->height;
