033c4795b84c54a48a218edaf8515c5ca7721618
Beeresh Gopal <gbeeresh@codeaurora.org>
2013-07-02 16:21:37 +0530

    msm: mdss: Add runtime writeback output format configuration
    Set writeback output format using metadata_ioctl before
    queuing buffer for write back from mdp. Default output
    format is VENUS. Writeback support RGB_565, RGB_888,
    ARGB_8888, XRGB_888 and VENUS as output format.
    
    Change-Id: I788b7f54d80a592ac8da07d10f6b292c4650e46c
    Signed-off-by: Beeresh Gopal <gbeeresh@codeaurora.org>
    Signed-off-by: Pawan Kumar <pavaku@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -608,6 +608,11 @@ struct mdss_mdp_ctl *mdss_mdp_ctl_mixer_switch(struct mdss_mdp_ctl *ctl,
 
 int mdss_mdp_ctl_update_fps(struct mdss_mdp_ctl *ctl, int fps);
 void mdss_mdp_underrun_clk_info(void);
+
+int mdss_mdp_wb_set_format(struct msm_fb_data_type *mfd, int dst_format);
+int mdss_mdp_wb_get_format(struct msm_fb_data_type *mfd,
+					struct mdp_mixer_cfg *mixer_cfg);
+
 #define mfd_to_mdp5_data(mfd) (mfd->mdp.private1)
 #define mfd_to_mdata(mfd) (((struct mdss_overlay_private *)\
 				(mfd->mdp.private1))->mdata)
