b3600c4ccd364f676ec074af7463741e2ccbeb75
Naseer Ahmed <naseer@codeaurora.org>
2013-09-04 19:33:21 -0400

    mdss: Reduce minimum ib bandwidth
    The minimum mdp ib for usecases such as portrait video is
    high, causing unnecessary power increase, so it is reduced to a
    optimal value for the purpose of both power saving and reducing
    chances of underruns.
    
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -35,7 +35,8 @@ extern bool first_update;
 /* 2.0 ab bus fudge factor */
 #define MDSS_MDP_BUS_FUDGE_FACTOR_AB(val) (val << 1)
 #endif
-#define MDSS_MDP_BUS_FLOOR_BW (3200000000ULL >> MDSS_MDP_BUS_FACTOR_SHIFT)
+#define MDSS_MDP_BUS_FLOOR_BW (1600000000ULL >> MDSS_MDP_BUS_FACTOR_SHIFT)
+
 /* 1.25 clock fudge factor */
 #define MDSS_MDP_CLK_FUDGE_FACTOR(val) (((val) * 5) / 4)
 
