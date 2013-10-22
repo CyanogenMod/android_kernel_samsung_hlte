02c6f3bda6c22a4a9adfb7d0a716a11be769992d
Xiaocheng Li <lix@codeaurora.org>
2013-10-22 10:25:58 +0000

    rtc: alarm: update power_on alarm setting logic
    The previous power_on alarm value should be checked to avoid
    cancelling the alarm by mistake.
    
    Signed-off-by: Xiaocheng Li <lix@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/rtc/alarm.c
+++ drivers/rtc/alarm.c
@@ -74,9 +74,19 @@ static long power_on_alarm;
 struct alarm *wakeup_alarm;
 
 static void alarm_shutdown(struct platform_device *dev);
-void set_power_on_alarm(long secs)
+void set_power_on_alarm(long secs, bool enable)
 {
-	power_on_alarm = secs;
+	if (enable) {
+		power_on_alarm = secs;
+	} else {
+		if (power_on_alarm && power_on_alarm != secs) {
+			pr_alarm(FLOW, "power-off alarm mismatch: \
+				previous=%ld, now=%ld\n",
+				power_on_alarm, secs);
+		}
+		else
+			power_on_alarm = 0;
+	}
 	alarm_shutdown(NULL);
 }
 
