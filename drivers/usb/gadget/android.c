39825b30b0eca5cbc54a7414782d06fc877506be
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2013-11-01 16:44:31 +0530

    USB: android: Schedule work only if suspended variable is changed
    currently as part of USB link status change event for U3 -> U0,
    DWC3 controller driver is not calling driver resume. This results
    in not updating suspended status in composite layer due to which
    disconnect, configured and then disconnect events are sent to userspace.
    This might cause problems for some userspace applications. Hence handle
    USB link status event for resume by reverting the commit b217f5007d
    ("USB: dwc3: Don't handle USB/Link Status change event for RESUME").
    Fix actual issue of disconnect sent during suspend/resume in android.c
    by scheduling work only if the suspended status is changed to notify
    userspace.
    
    CRs-Fixed: 569639
    Change-Id: If4fe6f3e8087d3bc972705ff40e4f94f597e2d09
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -3109,8 +3109,10 @@ static void android_suspend(struct usb_gadget *gadget)
 	unsigned long flags;
 
 	spin_lock_irqsave(&cdev->lock, flags);
-	dev->suspended = 1;
-	schedule_work(&dev->work);
+	if (!dev->suspended) {
+		dev->suspended = 1;
+		schedule_work(&dev->work);
+	}
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
 	composite_suspend(gadget);
@@ -3123,8 +3125,10 @@ static void android_resume(struct usb_gadget *gadget)
 	unsigned long flags;
 
 	spin_lock_irqsave(&cdev->lock, flags);
-	dev->suspended = 0;
-	schedule_work(&dev->work);
+	if (dev->suspended) {
+		dev->suspended = 0;
+		schedule_work(&dev->work);
+	}
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
 	composite_resume(gadget);
