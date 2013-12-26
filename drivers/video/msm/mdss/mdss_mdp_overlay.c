b747e2c14092e87c529edcec4bd7d7753441304f
Steve Kondik <shade@chemlab.org>
2013-12-26 00:47:30 -0800

    mdss: Fix merge derp

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -898,11 +898,11 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd)
 		if (pipe->back_buf.num_planes) {
 			buf = &pipe->back_buf;
 		} else if (ctl->play_cnt == 0 && pipe->front_buf.num_planes) {
+			pipe->params_changed++;
 			buf = &pipe->front_buf;
 		} else if (!pipe->params_changed) {
 			continue;
 		} else if (pipe->front_buf.num_planes) {
-			pipe->params_changed++;
 			buf = &pipe->front_buf;
 		} else {
 			pr_warn("pipe queue w/o buffer. unstaging layer\n");
