4250c5267b73631ed8c0f4cdc0ba929b82ca600a
Krishnankutty Kolathappilly <kkolat@codeaurora.org>
2014-01-15 11:36:39 -0800

    ASoC: msm: qdsp6v2: ASM changes to use stream id as token parameter
    Gapless compressed offload playback operates on two streams.
    Platform driver needs to know the stream for which the command
    callback is recieved. Use the token parameter in apr header to
    pass the stream for ASM commands for which DSP expects stream and
    session.
    
    Change-Id: Ibd8f5b6a52f57ccf8cc7eea15eb460b2f9796120
    Signed-off-by: Krishnankutty Kolathappilly <kkolat@codeaurora.org>

--- include/sound/q6asm-v2.h
+++ include/sound/q6asm-v2.h
@@ -72,6 +72,7 @@
 #define SYNC_IO_MODE	0x0001
 #define ASYNC_IO_MODE	0x0002
 #define COMPRESSED_IO	0x0040
+#define COMPRESSED_STREAM_IO	0x0080
 #define NT_MODE        0x0400
 
 #define NO_TIMESTAMP    0xFF00
@@ -404,5 +405,9 @@ int q6asm_set_dha(struct audio_client *ac,int *param);
 int q6asm_set_lrsm(struct audio_client *ac,int *param);
 int q6asm_set_sa_ep(struct audio_client *ac,int *param);
 int q6asm_get_sa_ep(struct audio_client *ac);
+
+/* Send the stream meta data to remove initial and trailing silence */
+int q6asm_stream_send_meta_data(struct audio_client *ac, uint32_t stream_id,
+		uint32_t initial_samples, uint32_t trailing_samples);
 
 #endif /* __Q6_ASM_H__ */
