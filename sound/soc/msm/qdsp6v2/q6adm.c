3966798857c34b64976db4cf00c8710502849c32
Subhash Chandra Bose Naripeddy <snariped@codeaurora.org>
2014-04-04 16:07:18 -0700

    ASoC: msm: qdsp6v2: Avoid DS1 and SRS topology on low latency path
    Currently, the low latency path go through tuning of the post
    processing with the default as well as third party post
    processing topologies such as DS1 and TruMedia topology. Avoid
    enabling the DS1 and TruMedia topology on low latency path
    retaining the tuning functionality on the default topology.
    
    CRs-Fixed: 643857
    Change-Id: I8ae6b4ecb0be530c8d72218d39498cff7f8a7fd9
    Signed-off-by: Subhash Chandra Bose Naripeddy <snariped@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6adm.c
+++ sound/soc/msm/qdsp6v2/q6adm.c
@@ -1225,6 +1225,10 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 			open.bit_width = ULL_SUPPORTED_BITS_PER_SAMPLE;
 			if(channel_mode > ULL_MAX_SUPPORTED_CHANNEL)
 				channel_mode = ULL_MAX_SUPPORTED_CHANNEL;
+		} else if (perf_mode == LOW_LATENCY_PCM_MODE) {
+			if ((open.topology_id == DOLBY_ADM_COPP_TOPOLOGY_ID) ||
+			    (open.topology_id == SRS_TRUMEDIA_TOPOLOGY_ID))
+				open.topology_id = DEFAULT_COPP_TOPOLOGY;
 		}
 		open.dev_num_channel = channel_mode & 0x00FF;
 		open.bit_width = bits_per_sample;
