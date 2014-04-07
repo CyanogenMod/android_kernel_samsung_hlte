6c8242bc3ec40d6ab871d3189619d5bf8a58ee0d
Ingrid Gallardo <ingridg@codeaurora.org>
2014-04-06 19:25:28 -0700

    msm: mdss add bw related events for command mode panels
    Add events to observe when bandwidth is requested,
    when bandwidth is needed and when bandwidth is released
    for command mode panels.
    
    Change-Id: I56b8ce28683696bee50e9cd10351076c49e4a84c
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -17,6 +17,7 @@
 #include "mdss_panel.h"
 #include "mdss_fb.h"
 #include "mdss_debug.h"
+#include "mdss_mdp_trace.h"
 
 #define VSYNC_EXPIRE_TICK 4
 
@@ -409,6 +410,7 @@ static void mdss_mdp_cmd_pingpong_done(void *arg)
 	} else
 		pr_err("%s: should not have pingpong interrupt!\n", __func__);
 
+	trace_mdp_cmd_pingpong_done(ctl, ctx->pp_num, ctx->koff_cnt);
 	pr_debug("%s: ctl_num=%d intf_num=%d ctx=%d kcnt=%d\n", __func__,
 		ctl->num, ctl->intf_num, ctx->pp_num, ctx->koff_cnt);
 
@@ -586,6 +588,8 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 		rc = wait_for_completion_timeout(
 				&ctx->pp_comp, msecs_to_jiffies(150));
 
+		trace_mdp_cmd_wait_pingpong(ctl->num, ctx->koff_cnt);
+
 		if (rc <= 0) {
 			WARN(1, "cmd kickoff timed out (rc = %d) ctl=%d\n",
 						rc, ctl->num);
@@ -667,6 +671,7 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 	spin_lock_irqsave(&ctx->clk_lock, flags);
 	ctx->koff_cnt++;
 	spin_unlock_irqrestore(&ctx->clk_lock, flags);
+	trace_mdp_cmd_kickoff(ctl->num, ctx->koff_cnt);
 
 	mdss_mdp_cmd_clk_on(ctx);
 
