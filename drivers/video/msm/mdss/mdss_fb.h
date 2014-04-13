f4520b7ca3ec008b62193408fe8fe541cbef6357
Ken Zhang <kenz@codeaurora.org>
2014-04-13 18:28:20 -0400

    msm: display: reduce display commit prepare waiting time
    Unblock overlay_set, prepare and buffer sync ioctls when
    commit thread finishes hardware programming.
    Use wait_for_kickoff nstead of pan_idle for these ioctls.
    
    Change-Id: Iefb6cbc390d2130926fdcb93688b4d92bfb26937
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    Signed-off-by: Neti Ravi Kumar <ravineti@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -222,8 +222,10 @@ struct msm_fb_data_type {
 	/* for non-blocking */
 	struct task_struct *disp_thread;
 	atomic_t commits_pending;
+	atomic_t kickoff_pending;
 	wait_queue_head_t commit_wait_q;
 	wait_queue_head_t idle_wait_q;
+	wait_queue_head_t kickoff_wait_q;
 	bool shutdown_pending;
 
 	struct task_struct *splash_thread;
