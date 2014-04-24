50910f848c300f54a4e21b9d4c46823683e8d1d2
arter97 <qkrwngud825@gmail.com>
2014-04-24 15:57:06 +0900

    gpio_keys: return early when flip_cover == gpio_flip_cover
    This prevents ghost turn-off or turn-on
    when there really wasn't a flip-cover event changed,
    but flip_cover_work() is just called.
    
    To prevent this, compare ddata->flip_cover with gpio_get_value(ddata->gpio_flip_cover)
    and proceed only on when those datas are different.

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -631,8 +631,12 @@ static void flip_cover_work(struct work_struct *work)
 	struct gpio_keys_drvdata *ddata =
 		container_of(work, struct gpio_keys_drvdata,
 				flip_cover_dwork.work);
+	bool flip_cover_tmp = gpio_get_value(ddata->gpio_flip_cover);
 
-	ddata->flip_cover = gpio_get_value(ddata->gpio_flip_cover);
+	if (ddata->flip_cover == flip_cover_tmp)
+		return;
+
+	ddata->flip_cover = flip_cover_tmp;
 	printk(KERN_DEBUG "[keys] %s : %d\n",
 		__func__, ddata->flip_cover);
 
