9822de2368bb0284313bbc24b4045bdd4aba9982
Janson Kang <temasek71@gmail.com>
2014-05-13 08:26:12 +0800

    Revert "nohz: stat: Fix CPU idle time accounting"
    This reverts commit ba978f373141d297ba7f42ea1e53430007832e68.
    
    Conflicts:
    
    	fs/proc/stat.c

--- fs/proc/stat.c
+++ fs/proc/stat.c
@@ -45,10 +45,7 @@ static cputime64_t get_iowait_time(int cpu)
 
 static u64 get_idle_time(int cpu)
 {
-	u64 idle, idle_time = -1ULL;
-
-	if (cpu_online(cpu))
-		idle_time = get_cpu_idle_time_us(cpu, NULL);
+	u64 idle, idle_time = get_cpu_idle_time_us(cpu, NULL);
 
 	if (idle_time == -1ULL)
 		/* !NO_HZ or cpu offline so we can rely on cpustat.idle */
