33045f3150ebc01b2e3a853e52cfb79969747f23
Steve Kondik <shade@chemlab.org>
2014-07-22 09:47:06 -0700

    Merge Samsung release N9005XXUFNF4 (Update 8)
    Change-Id: I36dd5d2d17648c3e9b33757f5659f5a59866e79e
    
    Conflicts:
    	arch/arm/kernel/smp.c
    	arch/arm/mach-msm/Kconfig
    	drivers/input/keyboard/gpio_keys.c
    	drivers/media/isdbt/Makefile
    
    Includes revert of this
    ARM: smp: BUG() if smp_send_reschedule() is called for an offline cpu
    https://github.com/temasek/android_kernel_samsung_hlte/commit/45682cf04d8f42ac245b70355689cbf0b27c17a9
    
    Patchset: 1
    http://review.cyanogenmod.org/#/c/68869/

--- arch/arm/mach-msm/board-sprd6500-modems.c
+++ arch/arm/mach-msm/board-sprd6500-modems.c
@@ -27,16 +27,29 @@
 #include <mach/gpiomux.h>
 
 
+#if defined(CONFIG_MACH_H3GDUOS)
+#define GPIO_GSM_PHONE_ON	135
+#define GPIO_PDA_ACTIVE		136
+#define GPIO_PHONE_ACTIVE	18
+#define GPIO_CP_DUMP_INT	73
+#define GPIO_AP_CP_INT1		124
+#define GPIO_AP_CP_INT2		125
+
+#define GPIO_UART_SEL		119
+#define GPIO_SIM_SEL		115
+#define ESC_SIM_DETECT_IRQ	125
+
+#elif defined(CONFIG_MACH_K3GDUOS_CTC)
 #define GPIO_GSM_PHONE_ON	127
 #define GPIO_PDA_ACTIVE		118
 #define GPIO_PHONE_ACTIVE	107
-#define GPIO_CP_DUMP_INT	119
-#define GPIO_AP_CP_INT1		0
-#define GPIO_AP_CP_INT2		0
+#define GPIO_AP_CP_INT1		0xFFF
+#define GPIO_AP_CP_INT2		0xFFF
 
 #define GPIO_UART_SEL		135
 #define GPIO_SIM_SEL		123
 #define ESC_SIM_DETECT_IRQ	123
+#endif
 
 #if defined(CONFIG_GSM_MODEM_GG_DUOS)
 /* gsm target platform data */
@@ -242,129 +255,6 @@ static struct modem_data gsm_modem_data = {
 #endif
 
 
-#if 0 //def CONFIG_OF
-
-void sprd6500_modem_cfg_gpio(struct platform_device *pdev)
-{
-	static const char *modem_name;
-	int gpio_cp_on, gpio_pda_active;
-	int gpio_ap_cp_int2, gpio_uart_sel;
-	int gpio_phone_active, gpio_cp_dump_int;
-
-	pr_err("%s:%d, debug info id=%d", __func__, __LINE__, pdev->id);
-	pr_err("%s:%d, debug info name=%s", __func__, __LINE__, pdev->name);
-
-	if (!pdev->dev.of_node)	{
-		pr_err("%s:%d, node not-found\n", __func__, __LINE__);
-		return -ENODEV;
-	}
-
-	modem_name = of_get_property(pdev->dev.of_node, "label", NULL);
-	if (!modem_name)
-		pr_err("%s:%d, sprd6500 modem name not specified\n",
-						__func__, __LINE__);
-	else
-		pr_err("%s: sprd6500 modem Name = %s\n", __func__, modem_name);
-
-
-	gpio_cp_on = of_get_named_gpio(pdev->dev.of_node,
-						 "samsung,gpio_cp_on", 0);
-
-	pr_err("gpio_cp_on : %d \n", gpio_cp_on);
-
-	if (!gpio_is_valid(gpio_cp_on)) {
-		pr_err("gpio_cp_on not specified\n");
-	} else {
-		gpio_tlmm_config(GPIO_CFG(gpio_cp_on, GPIOMUX_FUNC_GPIO,
-			GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-			GPIO_CFG_ENABLE);
-		gpio_set_value(gpio_cp_on, 0);
-	}
-
-	gpio_pda_active = of_get_named_gpio(pdev->dev.of_node,
-						 "samsung,gpio_pda_active", 0);
-
-	pr_err("gpio_pda_active : %d \n", gpio_pda_active);
-
-	if (!gpio_is_valid(gpio_pda_active)) {
-		pr_err("gpio_pda_active not specified\n");
-	} else {
-		gpio_tlmm_config(GPIO_CFG(gpio_pda_active, GPIOMUX_FUNC_GPIO,
-			GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-			GPIO_CFG_ENABLE);
-		gpio_set_value(gpio_pda_active, 0);
-	}
-
-	gpio_ap_cp_int2 = of_get_named_gpio(pdev->dev.of_node,
-						 "samsung,gpio_ap_cp_int2", 0);
-
-	pr_err("gpio_ap_cp_int2 : %d \n", gpio_ap_cp_int2);
-
-	if (!gpio_is_valid(gpio_ap_cp_int2)) {
-		pr_err("gpio_ap_cp_int2 not specified\n");
-	} else {
-		gpio_tlmm_config(GPIO_CFG(gpio_ap_cp_int2, GPIOMUX_FUNC_GPIO,
-			GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-			GPIO_CFG_ENABLE);
-		gpio_set_value(gpio_ap_cp_int2, 0);
-	}
-
-	gpio_uart_sel = of_get_named_gpio(pdev->dev.of_node,
-						 "samsung,gpio_uart_sel", 0);
-
-	pr_err("gpio_uart_sel : %d \n", gpio_uart_sel);
-
-	if (!gpio_is_valid(gpio_uart_sel)) {
-		pr_err("gpio_uart_sel not specified\n");
-	} else {
-		gpio_tlmm_config(GPIO_CFG(gpio_uart_sel, GPIOMUX_FUNC_GPIO,
-			GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-			GPIO_CFG_ENABLE);
-		gpio_set_value(gpio_uart_sel, 0);
-	}
-
-	gpio_phone_active = of_get_named_gpio(pdev->dev.of_node,
-						 "samsung,gpio_phone_active", 0);
-
-	pr_err("gpio_phone_active : %d \n", gpio_phone_active);
-
-	if (!gpio_is_valid(gpio_phone_active)) {
-		pr_err("gpio_phone_active not specified\n");
-	} else {
-		gpio_tlmm_config(GPIO_CFG(gpio_phone_active, GPIOMUX_FUNC_GPIO,
-			GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-			GPIO_CFG_ENABLE);
-	}
-
-	gpio_cp_dump_int = of_get_named_gpio(pdev->dev.of_node,
-						 "samsung,gpio_cp_dump_int", 0);
-
-	pr_err("gpio_cp_dump_int : %d \n", gpio_cp_dump_int);
-
-	if (!gpio_is_valid(gpio_cp_dump_int)) {
-		pr_err("gpio_cp_dump_int not specified\n");
-	} else {
-		gpio_tlmm_config(GPIO_CFG(gpio_cp_dump_int, GPIOMUX_FUNC_GPIO,
-			GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-			GPIO_CFG_ENABLE);
-	}
-
-	gsm_modem_data.gpio_cp_on = gpio_cp_on;
-	gsm_modem_data.gpio_pda_active = gpio_pda_active;
-	gsm_modem_data.gpio_phone_active = gpio_phone_active;
-	gsm_modem_data.gpio_cp_dump_int =gpio_cp_dump_int;
-	gsm_modem_data.gpio_ap_cp_int2 = gpio_ap_cp_int2;
-	gsm_modem_data.gpio_uart_sel = gpio_uart_sel;
-
-#if defined(CONFIG_SIM_DETECT)
-	gsm_modem_data.gpio_sim_sel= gpio_uart_sel;
-#endif
-
-	pr_info("sprd6500_modem_cfg_gpio done\n");
-}
-
-#else
-
 void sprd6500_modem_cfg_gpio(void)
 {
 	gpio_tlmm_config(GPIO_CFG(GPIO_GSM_PHONE_ON, GPIOMUX_FUNC_GPIO,
@@ -377,10 +267,12 @@ void sprd6500_modem_cfg_gpio(void)
 		GPIO_CFG_ENABLE);
 	gpio_set_value(GPIO_PDA_ACTIVE, 0);
 
+	if(gpio_is_valid(GPIO_AP_CP_INT2))	{
 	gpio_tlmm_config(GPIO_CFG(GPIO_AP_CP_INT2, GPIOMUX_FUNC_GPIO,
 		GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA),
 		GPIO_CFG_ENABLE);
 	gpio_set_value(GPIO_AP_CP_INT2, 0);
+	}
 
 	gpio_tlmm_config(GPIO_CFG(GPIO_UART_SEL, GPIOMUX_FUNC_GPIO,
 		GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA),
@@ -391,18 +283,15 @@ void sprd6500_modem_cfg_gpio(void)
 		GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
 		GPIO_CFG_ENABLE);
 
-	gpio_tlmm_config(GPIO_CFG(GPIO_CP_DUMP_INT, GPIOMUX_FUNC_GPIO,
-		GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-		GPIO_CFG_ENABLE);
-
+	if(gpio_is_valid(GPIO_AP_CP_INT1))	{
 	gpio_tlmm_config(GPIO_CFG(GPIO_AP_CP_INT1, GPIOMUX_FUNC_GPIO,
 		GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA),
 		GPIO_CFG_ENABLE);
+	}
 
 	gsm_modem_data.gpio_cp_on = GPIO_GSM_PHONE_ON;
 	gsm_modem_data.gpio_pda_active = GPIO_PDA_ACTIVE;
 	gsm_modem_data.gpio_phone_active = GPIO_PHONE_ACTIVE;
-	gsm_modem_data.gpio_cp_dump_int =GPIO_CP_DUMP_INT;
 	gsm_modem_data.gpio_ap_cp_int1 = GPIO_AP_CP_INT1;
 	gsm_modem_data.gpio_ap_cp_int2 = GPIO_AP_CP_INT2;
 	gsm_modem_data.gpio_uart_sel = GPIO_UART_SEL;
@@ -411,8 +300,6 @@ void sprd6500_modem_cfg_gpio(void)
 	pr_info("sprd6500_modem_cfg_gpio done\n");
 	pr_info("uart_sel : [%d]\n", gpio_get_value(GPIO_UART_SEL));
 }
-
-#endif
 
 static struct resource gsm_modem_res[] = {
 	[0] = {
