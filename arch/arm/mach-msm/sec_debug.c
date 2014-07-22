5583fba4085ef9f9b23518a16dfd45f71b7936d3
Steve Kondik <shade@chemlab.org>
2014-07-22 09:47:06 -0700

    Merge Samsung release N9005XXUFNF4 (Update 8)
    Change-Id: I36dd5d2d17648c3e9b33757f5659f5a59866e79e
    
    Conflicts:
    	arch/arm/kernel/smp.c
    	arch/arm/mach-msm/Kconfig
    	drivers/input/keyboard/gpio_keys.c
    	drivers/media/isdbt/Makefile
    
    Includes revert of this
    ARM: smp: BUG() if smp_send_reschedule() is called for an offline cpu
    https://github.com/temasek/android_kernel_samsung_hlte/commit/45682cf04d8f42ac245b70355689cbf0b27c17a9
    
    Patchset: 1
    http://review.cyanogenmod.org/#/c/68869/
    
    Conflicts:
    	arch/arm/configs/msm8974_sec_defconfig

--- arch/arm/mach-msm/sec_debug.c
+++ arch/arm/mach-msm/sec_debug.c
@@ -224,6 +224,8 @@ static unsigned pm8841_rev = 0;
 unsigned int sec_dbg_level;
 
 uint runtime_debug_val;
+static uint32_t tzapps_start_addr;
+static uint32_t tzapps_size;
 
 module_param_named(enable, enable, uint, 0644);
 module_param_named(enable_user, enable_user, uint, 0644);
@@ -1423,6 +1425,24 @@ void sec_debug_hw_reset(void)
 }
 EXPORT_SYMBOL(sec_debug_hw_reset);
 
+#ifdef CONFIG_SEC_PERIPHERAL_SECURE_CHK
+void sec_peripheral_secure_check_fail(void)
+{
+	sec_debug_set_upload_magic(0x77665507);
+	sec_debug_set_qc_dload_magic(0);
+        printk("sec_periphe\n");
+        pr_emerg("(%s) %s\n", __func__, sec_build_info);
+        pr_emerg("(%s) rebooting...\n", __func__);
+        flush_cache_all();
+        outer_flush_all();
+        msm_restart(0, "peripheral_hw_reset");
+
+        while (1)
+                ;
+}
+EXPORT_SYMBOL(sec_peripheral_secure_check_fail);
+#endif
+
 #ifdef CONFIG_SEC_DEBUG_LOW_LOG
 unsigned sec_debug_get_reset_reason(void)
 {
@@ -1745,6 +1765,12 @@ void sec_debug_save_cpu_freq_voltage(int cpu, int flag, unsigned long value)
 {
 }
 #endif
+void sec_debug_secure_app_addr_size(uint32_t addr,uint32_t size)
+{
+	tzapps_start_addr = addr;
+	tzapps_size =  size;
+}
+
 int sec_debug_subsys_init(void)
 {
 #ifdef CONFIG_SEC_DEBUG_VERBOSE_SUMMARY_HTML
@@ -1772,6 +1798,9 @@ int sec_debug_subsys_init(void)
 	}
 
 	memset(secdbg_subsys, 0, sizeof(secdbg_subsys));
+	
+	secdbg_subsys->secure_app_start_addr = tzapps_start_addr;
+	secdbg_subsys->secure_app_size = tzapps_size;
 
 	secdbg_krait = &secdbg_subsys->priv.krait;
 
