f8df3933a52dcaac28fa242533d97edf8e83e8aa
Terence Hampson <thampson@codeaurora.org>
2014-08-14 11:23:47 -0400

    msm: mdss: Prevent modification to image roi when scaling
    If pixel extension is enabled by scaler, and layer content is
    deinterlaced, the modifications to the layers region of interest
    to be used by mdp hw has already been modified. We should not
    further changing these values.
    
    Change-Id: I2c528b54ce07e90b66b56ef008b73b78e4227c9d
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -234,7 +234,8 @@ int mdss_mdp_overlay_req_check(struct msm_fb_data_type *mfd,
 			}
 		}
 
-		if (req->flags & MDP_DEINTERLACE) {
+		if ((req->flags & MDP_DEINTERLACE) &&
+					!req->scale.enable_pxl_ext) {
 			if (req->flags & MDP_SOURCE_ROTATED_90) {
 				if ((req->src_rect.w % 4) != 0) {
 					pr_err("interlaced rect not h/4\n");
@@ -615,7 +616,11 @@ int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 		}
 	}
 
-	if (pipe->flags & MDP_DEINTERLACE && !pipe->scale.enable_pxl_ext) {
+	/*
+	 * When scaling is enabled src crop and image
+	 * width and height is modified by user
+	 */
+	if ((pipe->flags & MDP_DEINTERLACE) && !pipe->scale.enable_pxl_ext) {
 		if (pipe->flags & MDP_SOURCE_ROTATED_90) {
 			pipe->src.x = DIV_ROUND_UP(pipe->src.x, 2);
 			pipe->src.x &= ~1;
