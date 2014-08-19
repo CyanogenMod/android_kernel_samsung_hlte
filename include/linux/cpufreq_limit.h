2eaf27ac1ceabad29801e2ce98b8a194a2db3d45
Janson Kang <temasek71@gmail.com>
2014-08-19 08:12:37 +0800

    cpufreq_limit: Define limits here as well

--- include/linux/cpufreq_limit.h
+++ include/linux/cpufreq_limit.h
@@ -14,11 +14,13 @@ struct cpufreq_limit_handle;
 
 #ifdef CONFIG_CPU_FREQ_LIMIT
 
-#if defined(CONFIG_ARCH_MSM8974PRO)
-#define MAX_FREQ_LIMIT	2457600
-#else
-#define MAX_FREQ_LIMIT	2265600
-#endif
+//#if defined(CONFIG_ARCH_MSM8974PRO)
+//#define MAX_FREQ_LIMIT	2457600
+//#else
+//#define MAX_FREQ_LIMIT	2265600
+//#endif
+//#define MIN_FREQ_LIMIT	300000
+#define MAX_FREQ_LIMIT	2496000
 #define MIN_FREQ_LIMIT	300000
 
 struct cpufreq_limit_handle *cpufreq_limit_get(unsigned long min_freq,
