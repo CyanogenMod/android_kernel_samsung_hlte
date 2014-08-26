ae76ce251ea5074db147c162577f4c5c7d5cae45
Janson Kang <temasek71@gmail.com>
2014-08-26 12:38:35 +0800

    Revert "rcu: Move PREEMPT_RCU preemption to switch_to() invocation"
    This reverts commit 192b28869cb2fd5f95a36b685927b1bfb7b6cd80.

--- include/linux/rcutiny.h
+++ include/linux/rcutiny.h
@@ -87,6 +87,10 @@ static inline void kfree_call_rcu(struct rcu_head *head,
 
 #ifdef CONFIG_TINY_RCU
 
+static inline void rcu_preempt_note_context_switch(void)
+{
+}
+
 static inline void exit_rcu(void)
 {
 }
@@ -98,6 +102,7 @@ static inline int rcu_needs_cpu(int cpu)
 
 #else /* #ifdef CONFIG_TINY_RCU */
 
+void rcu_preempt_note_context_switch(void);
 extern void exit_rcu(void);
 int rcu_preempt_needs_cpu(void);
 
@@ -111,6 +116,7 @@ static inline int rcu_needs_cpu(int cpu)
 static inline void rcu_note_context_switch(int cpu)
 {
 	rcu_sched_qs(cpu);
+	rcu_preempt_note_context_switch();
 }
 
 /*
