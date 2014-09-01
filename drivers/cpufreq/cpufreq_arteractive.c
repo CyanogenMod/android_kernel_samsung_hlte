90bf9144a42fd6db852f2ac2e8ee3bb533577085
arter97 <qkrwngud825@gmail.com>
2014-09-01 16:59:07 +0900

    arteractive: remove unnecessary tracer
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/cpufreq/cpufreq_arteractive.c
+++ drivers/cpufreq/cpufreq_arteractive.c
@@ -19,6 +19,7 @@
  * Version : 1.0 - Initial bring-up from SM-G900F_KK_Opensource_Update3.zip
  *                 Whitespace fix-ups
  *                 Apply upstream patches from https://github.com/android/kernel_common (branch android-3.4)
+ *                 Remove unnecessary tracer
  *
  */
 
@@ -37,9 +38,6 @@
 #include <linux/slab.h>
 #include <linux/kernel_stat.h>
 #include <asm/cputime.h>
-
-#define CREATE_TRACE_POINTS
-#include <trace/events/cpufreq_interactive.h>
 
 #define CONFIG_MODE_AUTO_CHANGE
 
@@ -556,10 +554,6 @@ static unsigned int check_mode(int cpu, unsigned int cur_mode, u64 now)
 			ret &= ~MULTI_MODE;
 	}
 
-	trace_cpufreq_interactive_mode(cpu, total_load,
-		time_in_single_enter, time_in_multi_enter,
-		time_in_single_exit, time_in_multi_exit, ret);
-
 	if (time_in_single_enter >= single_enter_time)
 		time_in_single_enter = 0;
 	if (time_in_multi_enter >= multi_enter_time)
@@ -713,9 +707,6 @@ static void cpufreq_interactive_timer(unsigned long data)
 	    new_freq > pcpu->target_freq &&
 	    now - pcpu->hispeed_validate_time <
 	    freq_to_above_hispeed_delay(pcpu->target_freq)) {
-		trace_cpufreq_interactive_notyet(
-			data, cpu_load, pcpu->target_freq,
-			pcpu->policy->cur, new_freq);
 		spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 		goto rearm;
 	}
@@ -748,9 +739,6 @@ static void cpufreq_interactive_timer(unsigned long data)
 	}
 	if (new_freq < pcpu->floor_freq) {
 		if (now - pcpu->floor_validate_time < mod_min_sample_time) {
-			trace_cpufreq_interactive_notyet(
-				data, cpu_load, pcpu->target_freq,
-				pcpu->policy->cur, new_freq);
 			spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 			goto rearm;
 		}
@@ -770,15 +758,9 @@ static void cpufreq_interactive_timer(unsigned long data)
 	}
 
 	if (pcpu->target_freq == new_freq) {
-		trace_cpufreq_interactive_already(
-			data, cpu_load, pcpu->target_freq,
-			pcpu->policy->cur, new_freq);
 		spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 		goto rearm_if_notmax;
 	}
-
-	trace_cpufreq_interactive_target(data, cpu_load, pcpu->target_freq,
-					 pcpu->policy->cur, new_freq);
 
 	pcpu->target_freq = new_freq;
 	spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
@@ -924,9 +906,6 @@ static int cpufreq_interactive_speedchange_task(void *data)
 				__cpufreq_driver_target(pcpu->policy,
 							max_freq,
 							CPUFREQ_RELATION_H);
-			trace_cpufreq_interactive_setspeed(cpu,
-						     pcpu->target_freq,
-						     pcpu->policy->cur);
 
 			up_read(&pcpu->enable_sem);
 		}
@@ -1425,11 +1404,9 @@ static ssize_t store_boost(struct kobject *kobj, struct attribute *attr,
 	boost_val = val;
 
 	if (boost_val) {
-		trace_cpufreq_interactive_boost("on");
 		cpufreq_interactive_boost();
 	} else {
 		boostpulse_endtime = ktime_to_us(ktime_get());
-		trace_cpufreq_interactive_unboost("off");
 	}
 
 	return count;
@@ -1448,7 +1425,6 @@ static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
 		return ret;
 
 	boostpulse_endtime = ktime_to_us(ktime_get()) + boostpulse_duration_val;
-	trace_cpufreq_interactive_boost("pulse");
 	cpufreq_interactive_boost();
 	return count;
 }
