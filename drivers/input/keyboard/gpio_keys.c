e4492cf61a3a9b2fcc64811fe6aba2f7c58d17e9
Janson Kang <temasek71@gmail.com>
2014-10-05 07:42:02 +0800

    Revert "gpio_keys: add a semaphore"
    This reverts commit c417eb23c33f96f48eb136af005e1bea2efb26d1.

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -85,7 +85,6 @@ static DECLARE_WORK(sync_system_work, sync_system);
 struct wake_lock sync_wake_lock;
 
 static bool suspended = false;
-static bool flip_cover_action = false;
 
 static void sync_system(struct work_struct *work)
 {
@@ -737,8 +736,7 @@ static void flip_cover_work(struct work_struct *work)
 		SW_FLIP, ddata->flip_cover);
 	input_sync(ddata->input);
 
-	if (ddata->flip_cover == 0 && !flip_cover_action && !suspended) {
-		flip_cover_action = true;
+	if (ddata->flip_cover == 0 && !suspended) {
 		pr_info("%s: flip cover closed. Going to sleep ...\n", __func__);
 		input_event(powerkey_device, EV_KEY, KEY_POWER, 1);
 		input_event(powerkey_device, EV_SYN, 0, 0);
@@ -746,10 +744,8 @@ static void flip_cover_work(struct work_struct *work)
 
 		input_event(powerkey_device, EV_KEY, KEY_POWER, 0);
 		input_event(powerkey_device, EV_SYN, 0, 0);
-		flip_cover_action = false;
 	}
-	if (ddata->flip_cover == 1 && !flip_cover_action && suspended) {
-		flip_cover_action = true;
+	if (ddata->flip_cover == 1 && suspended) {
 		pr_info("%s: flip cover opened. Waking up ...\n", __func__);
 		input_event(powerkey_device, EV_KEY, KEY_POWER, 1);
 		input_event(powerkey_device, EV_SYN, 0, 0);
@@ -757,7 +753,6 @@ static void flip_cover_work(struct work_struct *work)
 
 		input_event(powerkey_device, EV_KEY, KEY_POWER, 0);
 		input_event(powerkey_device, EV_SYN, 0, 0);
-		flip_cover_action = false;
 	}
 }
 #endif // CONFIG_SEC_FACTORY
