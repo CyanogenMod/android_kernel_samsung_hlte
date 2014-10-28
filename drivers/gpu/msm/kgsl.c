cbd7f62b90bf73c22f7598495d9b0fcb8bc846f2
Janson Kang <temasek71@gmail.com>
2014-10-28 09:29:16 +0800

    Revert "msm: kgsl: Cleanup the memory free list"
    This reverts commit 4b47c9eb5f0d9c6a7414279701c57b1fe3bfc14a.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -111,107 +111,51 @@ void kgsl_trace_regwrite(struct kgsl_device *device, unsigned int offset,
 }
 EXPORT_SYMBOL(kgsl_trace_regwrite);
 
-/*
- * The memfree list contains the last N blocks of memory that have been freed.
- * On a GPU fault we walk the list to see if the faulting address had been
- * recently freed and print out a message to that effect
- */
-
-#define MEMFREE_ENTRIES 512
-
-static DEFINE_SPINLOCK(memfree_lock);
-
-struct memfree_entry {
-	unsigned long gpuaddr;
-	unsigned long size;
-	pid_t pid;
-	unsigned int flags;
-};
-
-static struct {
-	struct memfree_entry *list;
-	int head;
-	int tail;
-} memfree;
-
-static int kgsl_memfree_init(void)
+int kgsl_memfree_hist_init(void)
 {
-	memfree.list = kzalloc(MEMFREE_ENTRIES * sizeof(struct memfree_entry),
-		GFP_KERNEL);
+	void *base;
 
-	return (memfree.list) ? 0 : -ENOMEM;
-}
-
-static void kgsl_memfree_exit(void)
-{
-	kfree(memfree.list);
-	memset(&memfree, 0, sizeof(memfree));
-}
-
-int kgsl_memfree_find_entry(pid_t pid, unsigned long *gpuaddr,
-	unsigned long *size, unsigned int *flags)
-{
-	int ptr;
-
-	if (memfree.list == NULL)
-		return 0;
-
-	spin_lock(&memfree_lock);
-
-	ptr = memfree.head - 1;
-	if (ptr < 0)
-		ptr = MEMFREE_ENTRIES - 1;
-
-	/* Walk backwards through the list looking for the last match  */
-	while (ptr != memfree.tail) {
-		struct memfree_entry *entry = &memfree.list[ptr];
-
-		if ((entry->pid == pid) &&
-			(*gpuaddr >= entry->gpuaddr &&
-			 *gpuaddr < (entry->gpuaddr + entry->size))) {
-			*gpuaddr = entry->gpuaddr;
-			*flags = entry->flags;
-			*size = entry->size;
-
-			spin_unlock(&memfree_lock);
-			return 1;
-		}
-
-		ptr = ptr - 1;
-
-		if (ptr < 0)
-			ptr = MEMFREE_ENTRIES - 1;
-	}
-
-	spin_unlock(&memfree_lock);
+	base = kzalloc(KGSL_MEMFREE_HIST_SIZE, GFP_KERNEL);
+	kgsl_driver.memfree_hist.base_hist_rb = base;
+	if (base == NULL)
+		return -ENOMEM;
+	kgsl_driver.memfree_hist.size = KGSL_MEMFREE_HIST_SIZE;
+	kgsl_driver.memfree_hist.wptr = base;
 	return 0;
 }
 
-static void kgsl_memfree_add(pid_t pid, unsigned int gpuaddr,
-		unsigned int size, int flags)
-
+void kgsl_memfree_hist_exit(void)
 {
-	struct memfree_entry *entry;
+	kfree(kgsl_driver.memfree_hist.base_hist_rb);
+	kgsl_driver.memfree_hist.base_hist_rb = NULL;
+}
 
-	if (memfree.list == NULL)
+void kgsl_memfree_hist_set_event(unsigned int pid, unsigned int gpuaddr,
+			unsigned int size, int flags)
+{
+	struct kgsl_memfree_hist_elem *p;
+
+	void *base = kgsl_driver.memfree_hist.base_hist_rb;
+	int rbsize = kgsl_driver.memfree_hist.size;
+
+	if (base == NULL)
 		return;
 
-	spin_lock(&memfree_lock);
+	mutex_lock(&kgsl_driver.memfree_hist_mutex);
+	p = kgsl_driver.memfree_hist.wptr;
+	p->pid = pid;
+	p->gpuaddr = gpuaddr;
+	p->size = size;
+	p->flags = flags;
 
-	entry = &memfree.list[memfree.head];
-
-	entry->pid = pid;
-	entry->gpuaddr = gpuaddr;
-	entry->size = size;
-	entry->flags = flags;
-
-	memfree.head = (memfree.head + 1) % MEMFREE_ENTRIES;
-
-	if (memfree.head == memfree.tail)
-		memfree.tail = (memfree.tail + 1) % MEMFREE_ENTRIES;
-
-	spin_unlock(&memfree_lock);
+	kgsl_driver.memfree_hist.wptr++;
+	if ((void *)kgsl_driver.memfree_hist.wptr >= base+rbsize) {
+		kgsl_driver.memfree_hist.wptr =
+			(struct kgsl_memfree_hist_elem *)base;
+	}
+	mutex_unlock(&kgsl_driver.memfree_hist_mutex);
 }
+
 
 /* kgsl_get_mem_entry - get the mem_entry structure for the specified object
  * @device - Pointer to the device structure
@@ -2358,30 +2302,7 @@ static long kgsl_ioctl_drawctxt_destroy(struct kgsl_device_private *dev_priv,
 	return result;
 }
 
-static long _sharedmem_free_entry(struct kgsl_mem_entry *entry)
-{
-	if (!kgsl_mem_entry_set_pend(entry)) {
-		kgsl_mem_entry_put(entry);
-		return -EBUSY;
-	}
-
-	trace_kgsl_mem_free(entry);
-
-	kgsl_memfree_add(entry->priv->pid, entry->memdesc.gpuaddr,
-		entry->memdesc.size, entry->memdesc.flags);
-
-	/*
-	 * First kgsl_mem_entry_put is for the reference that we took in
-	 * this function when calling kgsl_sharedmem_find, second one is
-	 * to free the memory since this is a free ioctl
-	 */
-	kgsl_mem_entry_put(entry);
-	kgsl_mem_entry_put(entry);
-
-	return 0;
-}
-
-long kgsl_ioctl_sharedmem_free(struct kgsl_device_private *dev_priv,
+static long kgsl_ioctl_sharedmem_free(struct kgsl_device_private *dev_priv,
 					unsigned int cmd, void *data)
 {
 	struct kgsl_sharedmem_free *param = data;
@@ -2394,11 +2315,29 @@ long kgsl_ioctl_sharedmem_free(struct kgsl_device_private *dev_priv,
 				param->gpuaddr);
 		return -EINVAL;
 	}
+	if (!kgsl_mem_entry_set_pend(entry)) {
+		kgsl_mem_entry_put(entry);
+		return -EBUSY;
+	}
 
-	return _sharedmem_free_entry(entry);
+	trace_kgsl_mem_free(entry);
+
+	kgsl_memfree_hist_set_event(entry->priv->pid,
+				    entry->memdesc.gpuaddr,
+				    entry->memdesc.size,
+				    entry->memdesc.flags);
+
+	/*
+	 * First kgsl_mem_entry_put is for the reference that we took in
+	 * this function when calling kgsl_sharedmem_find, second one is
+	 * to free the memory since this is a free ioctl
+	 */
+	kgsl_mem_entry_put(entry);
+	kgsl_mem_entry_put(entry);
+	return 0;
 }
 
-long kgsl_ioctl_gpumem_free_id(struct kgsl_device_private *dev_priv,
+static long kgsl_ioctl_gpumem_free_id(struct kgsl_device_private *dev_priv,
 					unsigned int cmd, void *data)
 {
 	struct kgsl_gpumem_free_id *param = data;
@@ -2412,7 +2351,26 @@ long kgsl_ioctl_gpumem_free_id(struct kgsl_device_private *dev_priv,
 		return -EINVAL;
 	}
 
-	return _sharedmem_free_entry(entry);
+	if (!kgsl_mem_entry_set_pend(entry)) {
+		kgsl_mem_entry_put(entry);
+		return -EBUSY;
+	}
+
+	trace_kgsl_mem_free(entry);
+
+	kgsl_memfree_hist_set_event(entry->priv->pid,
+				    entry->memdesc.gpuaddr,
+				    entry->memdesc.size,
+				    entry->memdesc.flags);
+
+	/*
+	 * First kgsl_mem_entry_put is for the reference that we took in
+	 * this function when calling kgsl_sharedmem_find_id, second one is
+	 * to free the memory since this is a free ioctl
+	 */
+	kgsl_mem_entry_put(entry);
+	kgsl_mem_entry_put(entry);
+	return 0;
 }
 
 static struct vm_area_struct *kgsl_get_vma_from_start_addr(unsigned int addr)
@@ -3953,6 +3911,8 @@ struct kgsl_driver kgsl_driver  = {
 	.process_mutex = __MUTEX_INITIALIZER(kgsl_driver.process_mutex),
 	.ptlock = __SPIN_LOCK_UNLOCKED(kgsl_driver.ptlock),
 	.devlock = __MUTEX_INITIALIZER(kgsl_driver.devlock),
+	.memfree_hist_mutex =
+		__MUTEX_INITIALIZER(kgsl_driver.memfree_hist_mutex),
 	/*
 	 * Full cache flushes are faster than line by line on at least
 	 * 8064 and 8974 once the region to be flushed is > 16mb.
@@ -4300,7 +4260,7 @@ static void kgsl_core_exit(void)
 		kgsl_driver.class = NULL;
 	}
 
-	kgsl_memfree_exit();
+	kgsl_memfree_hist_exit();
 	unregister_chrdev_region(kgsl_driver.major, KGSL_DEVICE_MAX);
 }
 
@@ -4372,7 +4332,8 @@ static int __init kgsl_core_init(void)
 			goto err;
 	}
 
-	kgsl_memfree_init();
+	if (kgsl_memfree_hist_init())
+		KGSL_CORE_ERR("failed to init memfree_hist");
 
 	return 0;
 
