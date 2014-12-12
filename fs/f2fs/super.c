a1c0c0598b35819e086f2d256f5f0131dcc5ca2a
arter97 <qkrwngud825@gmail.com>
2014-12-12 12:25:33 +0900

    f2fs: add nodiscard option
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -42,6 +42,7 @@ enum {
 	Opt_gc_background,
 	Opt_disable_roll_forward,
 	Opt_discard,
+	Opt_nodiscard,
 	Opt_noheap,
 	Opt_user_xattr,
 	Opt_nouser_xattr,
@@ -62,6 +63,7 @@ static match_table_t f2fs_tokens = {
 	{Opt_gc_background, "background_gc=%s"},
 	{Opt_disable_roll_forward, "disable_roll_forward"},
 	{Opt_discard, "discard"},
+	{Opt_nodiscard, "nodiscard"},
 	{Opt_noheap, "no_heap"},
 	{Opt_user_xattr, "user_xattr"},
 	{Opt_nouser_xattr, "nouser_xattr"},
@@ -288,6 +290,9 @@ static int parse_options(struct super_block *sb, char *options)
 			break;
 		case Opt_discard:
 			set_opt(sbi, DISCARD);
+			break;
+		case Opt_nodiscard:
+			clear_opt(sbi, DISCARD);
 			break;
 		case Opt_noheap:
 			set_opt(sbi, NOHEAP);
