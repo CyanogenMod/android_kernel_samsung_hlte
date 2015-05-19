d57810940b7dc20a35576e031ad9fcf4a2948415
Patrick Riphagen <patrick.riphagen@xsens.com>
2015-05-19 10:03:01 +0200

    USB: serial: ftdi_sio: Add support for a Motion Tracker Development Board
    commit 1df5b888f54070a373a73b34488cc78c2365b7b4 upstream.
    
    This adds support for new Xsens device, Motion Tracker Development Board,
    using Xsens' own Vendor ID
    
    Signed-off-by: Patrick Riphagen <patrick.riphagen@xsens.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Zefan Li <lizefan@huawei.com>
    
    Conflicts:
    
    	drivers/usb/serial/ftdi_sio.c
    	drivers/usb/serial/ftdi_sio_ids.h

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -145,8 +145,16 @@
 /*
  * Xsens Technologies BV products (http://www.xsens.com).
  */
-#define XSENS_CONVERTER_0_PID	0xD388
-#define XSENS_CONVERTER_1_PID	0xD389
+#define XSENS_VID		0x2639
+#define XSENS_AWINDA_STATION_PID 0x0101
+#define XSENS_AWINDA_DONGLE_PID 0x0102
+#define XSENS_MTW_PID		0x0200	/* Xsens MTw */
+#define XSENS_MTDEVBOARD_PID	0x0300	/* Motion Tracker Development Board */
+#define XSENS_CONVERTER_PID	0xD00D	/* Xsens USB-serial converter */
+
+/* Xsens devices using FTDI VID */
+#define XSENS_CONVERTER_0_PID	0xD388	/* Xsens USB converter */
+#define XSENS_CONVERTER_1_PID	0xD389	/* Xsens Wireless Receiver */
 #define XSENS_CONVERTER_2_PID	0xD38A
 #define XSENS_CONVERTER_3_PID	0xD38B
 #define XSENS_CONVERTER_4_PID	0xD38C
