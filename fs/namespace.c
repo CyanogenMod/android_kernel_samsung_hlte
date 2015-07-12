5c12cd703be5b7565ee858e58f3fe12b37f077d8
Thierry Strudel <tstrudel@google.com>
2015-07-11 22:45:44 -0700

    Revert "vfs: umount_tree() might be called on subtree that had never made it"
    This reverts commit 61fc3d0b2f95e232c7e4021b9e15790377efb80b.
    
    Change-Id: Ib1c690354dc3b48fca4ea4a4d7c881fd49bfb2f7

--- fs/namespace.c
+++ fs/namespace.c
@@ -1076,8 +1076,6 @@ void umount_tree(struct mount *mnt, int propagate, struct list_head *kill)
 		list_del_init(&p->mnt_expire);
 		list_del_init(&p->mnt_list);
 		__touch_mnt_namespace(p->mnt_ns);
-		if (p->mnt_ns)
-			__mnt_make_shortterm(p);
 		p->mnt_ns = NULL;
 		if (mnt_has_parent(p)) {
 			p->mnt_parent->mnt_ghosts++;
