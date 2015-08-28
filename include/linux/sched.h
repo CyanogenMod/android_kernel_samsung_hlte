cdb80b971210d6cd7c44e328c9b5191fe4342067
Janson Kang <temasek71@gmail.com>
2015-08-28 13:36:59 +0800

    Revert "thread_info: Replace usages of test_thread_flag with relaxed version"
    This reverts commit 11b2a984212081397a3844034ca687345bd7413b.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2662,7 +2662,7 @@ static inline int test_and_clear_tsk_thread_flag(struct task_struct *tsk, int fl
 
 static inline int test_tsk_thread_flag(struct task_struct *tsk, int flag)
 {
-	return test_ti_thread_flag_relaxed(task_thread_info(tsk), flag);
+	return test_ti_thread_flag(task_thread_info(tsk), flag);
 }
 
 static inline void set_tsk_need_resched(struct task_struct *tsk)
@@ -2713,7 +2713,7 @@ static inline int signal_pending_state(long state, struct task_struct *p)
 
 static inline int need_resched(void)
 {
-	return unlikely(test_thread_flag_relaxed(TIF_NEED_RESCHED));
+	return unlikely(test_thread_flag(TIF_NEED_RESCHED));
 }
 
 /*
