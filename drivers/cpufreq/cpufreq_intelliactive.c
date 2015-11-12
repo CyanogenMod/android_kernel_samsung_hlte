4565f23aa1b13ef651ca16d61b095b14b68b3f88
Janson Kang <temasek71@gmail.com>
2015-11-12 09:07:55 +0800

    intelliactive: Drop go_hispeed_load to 90

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -70,7 +70,7 @@ static struct mutex gov_lock;
 static unsigned int hispeed_freq = 1190400;
 
 /* Go to hi speed when CPU load at or above this value. */
-#define DEFAULT_GO_HISPEED_LOAD 99
+#define DEFAULT_GO_HISPEED_LOAD 90
 static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
 
 /* Sampling down factor to be applied to min_sample_time at max freq */
