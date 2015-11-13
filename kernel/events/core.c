c5272a73449c14215a65c81f2eea6935a112025c
Andrew Bartholomew <andrewb03@gmail.com>
2015-11-13 13:21:40 -0500

    perf: fix mismerge
    Change-Id: I159e4ab114cdca2154e6b12240122dee90fe8644

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1299,6 +1299,11 @@ retry:
 	 */
 	if (ctx->is_active) {
 		raw_spin_unlock_irq(&ctx->lock);
+		/*
+		 * Reload the task pointer, it might have been changed by
+		 * a concurrent perf_event_context_sched_out().
+		 */
+		task = ctx->task;
 		goto retry;
 	}
 
@@ -1728,11 +1733,6 @@ retry:
 	 */
 	if (ctx->is_active) {
 		raw_spin_unlock_irq(&ctx->lock);
-		/*
-		 * Reload the task pointer, it might have been changed by
-		 * a concurrent perf_event_context_sched_out().
-		 */
-		task = ctx->task;
 		/*
 		 * Reload the task pointer, it might have been changed by
 		 * a concurrent perf_event_context_sched_out().
