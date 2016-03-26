3550b3c274ffb9028f91829e5a74e11f7a790389
Janson Kang <temasek71@gmail.com>
2016-03-26 16:52:14 +0800

    Revert "qcom:rpm-smd: Add debug ftrace events"
    This reverts commit 2856c257e677d0e935b3b834929acb5c15d8dfe8.

--- arch/arm/mach-msm/include/mach/trace_rpm_smd.h
+++ arch/arm/mach-msm/include/mach/trace_rpm_smd.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, 2014-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -18,94 +18,60 @@
 
 #include <linux/tracepoint.h>
 
-TRACE_EVENT(rpm_smd_ack_recvd,
+TRACE_EVENT(rpm_ack_recd,
 
-	TP_PROTO(unsigned int irq, unsigned int msg_id, int errno),
+	TP_PROTO(unsigned int irq, unsigned int msg_id),
 
-	TP_ARGS(irq, msg_id, errno),
+	TP_ARGS(irq, msg_id),
 
 	TP_STRUCT__entry(
 		__field(int, irq)
 		__field(int, msg_id)
-		__field(int, errno)
 	),
 
 	TP_fast_assign(
 		__entry->irq = irq;
 		__entry->msg_id = msg_id;
-		__entry->errno = errno;
 	),
 
-	TP_printk("ctx:%s msg_id:%d errno:%08x",
+	TP_printk("ctx:%s id:%d",
 		__entry->irq ? "noslp" : "sleep",
-		__entry->msg_id,
-		__entry->errno)
+		__entry->msg_id)
 );
 
-TRACE_EVENT(rpm_smd_interrupt_notify,
+TRACE_EVENT(rpm_send_message,
 
-	TP_PROTO(char *dummy),
+	TP_PROTO(unsigned int irq, unsigned int set, unsigned int rsc_type,
+		unsigned int rsc_id, unsigned int msg_id),
 
-	TP_ARGS(dummy),
-
-	TP_STRUCT__entry(
-		__field(char *, dummy)
-	),
-
-	TP_fast_assign(
-		__entry->dummy = dummy;
-	),
-
-	TP_printk("%s", __entry->dummy)
-);
-
-DECLARE_EVENT_CLASS(rpm_send_msg,
-
-	TP_PROTO(unsigned int msg_id, unsigned int rsc_type,
-		unsigned int rsc_id),
-
-	TP_ARGS(msg_id, rsc_type, rsc_id),
+	TP_ARGS(irq, set, rsc_type, rsc_id, msg_id),
 
 	TP_STRUCT__entry(
-		__field(u32, msg_id)
+		__field(u32, irq)
+		__field(u32, set)
 		__field(u32, rsc_type)
 		__field(u32, rsc_id)
+		__field(u32, msg_id)
 		__array(char, name, 5)
 	),
 
 	TP_fast_assign(
-		__entry->msg_id = msg_id;
+		__entry->irq	= irq;
 		__entry->name[4] = 0;
+		__entry->set = set;
 		__entry->rsc_type = rsc_type;
 		__entry->rsc_id = rsc_id;
+		__entry->msg_id = msg_id;
 		memcpy(__entry->name, &rsc_type, sizeof(uint32_t));
 
 	),
 
-	TP_printk("msg_id:%d, rsc_type:0x%08x(%s), rsc_id:0x%08x",
-			__entry->msg_id,
+	TP_printk("ctx:%s set:%s rsc_type:0x%08x(%s), rsc_id:0x%08x, id:%d",
+			__entry->irq ? "noslp" : "sleep",
+			__entry->set ? "slp" : "act",
 			__entry->rsc_type, __entry->name,
-			__entry->rsc_id)
+			__entry->rsc_id, __entry->msg_id)
 );
-
-DEFINE_EVENT(rpm_send_msg, rpm_smd_sleep_set,
-	TP_PROTO(unsigned int msg_id, unsigned int rsc_type,
-		unsigned int rsc_id),
-	TP_ARGS(msg_id, rsc_type, rsc_id)
-);
-
-DEFINE_EVENT(rpm_send_msg, rpm_smd_send_sleep_set,
-	TP_PROTO(unsigned int msg_id, unsigned int rsc_type,
-		unsigned int rsc_id),
-	TP_ARGS(msg_id, rsc_type, rsc_id)
-);
-
-DEFINE_EVENT(rpm_send_msg, rpm_smd_send_active_set,
-	TP_PROTO(unsigned int msg_id, unsigned int rsc_type,
-		unsigned int rsc_id),
-	TP_ARGS(msg_id, rsc_type, rsc_id)
-);
-
 #endif
 #undef TRACE_INCLUDE_PATH
 #define TRACE_INCLUDE_PATH mach
