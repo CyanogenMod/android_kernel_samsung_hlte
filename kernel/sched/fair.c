1b0af91d48713fc0b27502a9557657873b66861c
Janson Kang <temasek71@gmail.com>
2016-03-28 08:19:47 +0800

    Revert "sched: Add sysinterface for GENTLE_FAIR_SLEEPERS"
    This reverts commit 9da781443ebe6871ce6393fafa20023933065ecc.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1087,12 +1087,6 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
-static unsigned int Lgentle_fair_sleepers = 1;
-void relay_gfs(unsigned int gfs)
-{
-	Lgentle_fair_sleepers = gfs;
-}
-
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -1115,7 +1109,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * Halve their sleep time's effect, to allow
 		 * for a gentler effect of sleepers:
 		 */
-		if (Lgentle_fair_sleepers)
+		if (sched_feat(GENTLE_FAIR_SLEEPERS))
 			thresh >>= 1;
 
 		vruntime -= thresh;
