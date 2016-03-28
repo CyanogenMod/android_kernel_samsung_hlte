7a95d0d137f326b829cdf992a9c9ea78d7f971e7
Janson Kang <temasek71@gmail.com>
2016-03-28 08:21:39 +0800

    Revert "timer: optimize apply_slack()"
    This reverts commit ce32f8f0b1e1a29b6656b3fa30221c9ac1a512a4.

--- kernel/timer.c
+++ kernel/timer.c
@@ -845,7 +845,7 @@ unsigned long apply_slack(struct timer_list *timer, unsigned long expires)
 	if (mask == 0)
 		return expires;
 
-	bit = __fls(mask);
+	bit = find_last_bit(&mask, BITS_PER_LONG);
 
 	mask = (1UL << bit) - 1;
 
